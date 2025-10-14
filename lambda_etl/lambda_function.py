12dab8feed1e3a79fe7a14e06ec525f4e35af959
DacheOA <dachely.otero.argote@gmail.com>
2025-10-13 18:23:08 -0600

    Storage in DynamoDB

--- lambda_etl/lambda_function.py
+++ lambda_etl/lambda_function.py
@@ -2,6 +2,11 @@ import boto3
 import pandas as pd
 import io
 from decimal import Decimal, InvalidOperation
+import os
+
+table_name = os.environ.get("TABLE_NAME", "El nombre de la tabla")
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
 
@@ -13,11 +18,12 @@ def lambda_handler(event, context):
     # Read the CSV file from S3 into a pandas DataFrame
     csv_file = s3.get_object(Bucket = bucket, Key = key)
     df = pd.read_csv(io.BytesIO(csv_file['Body'].read()))
-    print('df: ', df)
+    #print('df head: ', df.head())
 
     # Normalize dates to timestamp
     #df['timestamp'] = pd.to_datetime(df['timestamp']).astype(int) // 10**9
     df['timestamp'] = pd.to_datetime(df['timestamp'])
+
 
     #Filter transactions with status = approved
     df_approved = df[df['status'] == 'approved']
@@ -28,4 +34,29 @@ def lambda_handler(event, context):
         total_amount = pd.NamedAgg(column= 'amount', aggfunc= 'sum')
     ).reset_index()
 
-    print('grouped: ', grouped)
+    #print('grouped: ', grouped)
+
+    # Insert the processed data into DynamoDB
+    for _, row in grouped.iterrows():
+        id = str(int(row['user_id']))
+        count = int(row['transactions_count'])
+        amount = row['total_amount']
+
+        try:
+            amount_decimal = Decimal(str(amount))
+        except InvalidOperation:
+            amount_decimal = Decimal(0)
+
+        table.put_item(
+            Item = {
+                'user_id': id,
+                'transactions_count': count,
+                'total_amount': amount_decimal
+            }
+        )
+    print('Ok')
+
+    return {
+        'statusCode': 200,
+        'body': 'Data successfull inserted into DynamoDB'
+    }
--- stori_technical_challenge/stori_technical_challenge_stack.py
+++ stori_technical_challenge/stori_technical_challenge_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_lambda as _lambda,
     aws_s3_notifications as s3n,
+    aws_dynamodb as dynamodb
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -15,18 +16,33 @@ class StoriTechnicalChallengeStack(Stack):
         # Create an S3 bucket
         data_bucket = s3.Bucket(self, "DataBucket")
 
+        # Create a Dynamodb TableV2 to store processed data
+        results_table = dynamodb.TableV2(
+            self, "ResultsTable",
+            partition_key=dynamodb.Attribute(
+                name="user_id", 
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing= dynamodb.Billing.on_demand()
+        )
+
         # Create a Lambda function
         etl_lambda = _lambda.Function(
             self, "ETLLambda",
             runtime=_lambda.Runtime.PYTHON_3_9,
             handler="lambda_function.lambda_handler",
-            code=_lambda.Code.from_asset("lambda_etl.zip")
+            code=_lambda.Code.from_asset("lambda_etl.zip"),
+            environment= {"TABLE_NAME": results_table.table_name}
         )
 
         # Grant the Lambda function read permissions to the S3 bucket
         data_bucket.grant_read(etl_lambda)
 
+        # Add an event notification to the S3 bucket to trigger the Lambda function on object creation
         data_bucket.add_event_notification(
             s3.EventType.OBJECT_CREATED,
             s3n.LambdaDestination(etl_lambda),
         )
+
+        # Grant the Lambda function write permissions to the DynamoDB table
+        results_table.grant_write_data(etl_lambda)
